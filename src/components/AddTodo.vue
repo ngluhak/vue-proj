e6231877cd211a6ee2c1990e60274ea229d4e1b0
ngluhak <72704908+ngluhak@users.noreply.github.com>
2020-11-29 16:14:19 +0100

    first commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,34 @@
+<template>
+  <form>
+      <b-input v-model="name" placeholder="upisi ime todo itema"></b-input>
+        
+      <b-button >Novi todo</b-button>
+  </form>
+</template>
+
+<script>
+export default {
+    data (){
+        return{
+        }
+    }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+   form{
+       display:flex;
+       margin-top: 20px;
+       padding-top: 2px;
+       border-top: 1px solid #EFEFEF;
+   }
+   div{
+       flex:1;
+       margin-right: 20px;
+   }
+    button{
+        margin-top: auto;
+    }
+</style>
