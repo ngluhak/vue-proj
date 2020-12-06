96fe3695008002cd3806f4ab5445f05d117d3130
ngluhak <72704908+ngluhak@users.noreply.github.com>
2020-12-07 00:26:47 +0100

    edited the img and carousel

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,44 +5,43 @@
 
 
 
-  <nav class="level navigation">
-  <!-- Left side -->
-  <div class="level-left">
-    <div class="level-item">
-      <a href="#" class="logo">Indoor Garden</a>
-    </div>
-  </div>
-
-  <!-- Right side -->
-  <div class="level-right">
-    <nav class="right">
+<div class="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-s-12">
+					<a href="About.vue" class="logo">Indoor Garden</a>
+				</div>
+				<div class="col-md-8 col-s-12 align-self-center">
+					<nav class="right">
 						<ul>
-							<li><a href="#">HOME</a></li>
-							<li><a href="shop.html">SHOP</a></li>
+							<li><a href="index.html">HOME</a></li>
+							<li><a href="#">SHOP</a></li>
 							<li><a href="kontakt.php">CONTACT</a></li>
 						</ul>
-		</nav>
-  </div>
-</nav>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
 	
 	<header>
-		<h1>Welcome to Indoor Garden</h1>
+		<h1>Welcome to <strong style="color: white;">Indoor Garden</strong></h1>
 	</header>
 	
 	<div class="container center-text">
 		<div class="row">
 			<div class="col-md-4 col-s-12">
-				<i class="fas fa-clock"></i>
+				<i class="fas fa-clock" style="font-size: 40px; margin-bottom: 20px; color: #85a856;"></i>
 				<h3 class="razmak">Fast delivery</h3>
 			</div>
 			<div class="col-md-4 col-s-12">
-				<i class="fas fa-leaf"></i>
+				<i class="fas fa-leaf" style="font-size: 40px; margin-bottom: 20px; color: #85a856;"></i>
 				<h3 class="razmak">High-quality</h3>
 			</div>
 			<div class="col-md-4 col-s-12">
-				<i class="fas fa-euro-sign"></i>
+				<i class="fas fa-euro-sign" style="font-size: 40px; margin-bottom: 20px; color: #85a856;"></i>
 				<h3>Not expensive</h3>
 			</div>
 		</div>		
@@ -60,16 +59,16 @@
 	<div class="container slike">
 		<div class="row">
 			<div class="col-md-3 col-s-12">
-                    <img src="img/prvi.jpg" alt="image" />
+                    <img :src="require('@/assets/prvi.jpg')" alt="image" />
             </div>
 			<div class="col-md-3 col-s-12">
-                    <img src="img/drugi.jpg" alt="image" />
+                     <img :src="require('@/assets/drugi.jpg')" alt="image" />
             </div>
 			<div class="col-md-3 col-s-12">
-                    <img src="img/treci.jpg" alt="image" />
+                    <img :src="require('@/assets/treci.jpg')" alt="image" />
             </div>
 			<div class="col-md-3 col-s-12">
-                    <img src="img/cetvrti.jpg" alt="image" />
+                    <img :src="require('@/assets/cetvrti.jpg')" alt="image" />
             </div>
 		</div>
 	</div>
@@ -86,15 +85,27 @@
 	<div class="container center-text">
 		<div class="row">
 			<div class="col-md-6 col-s-12">
-				<b-carousel>
-          <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-              <section :class="`hero is-medium is-${carousel.src}`">
-                  <div class="hero-body has-text-centered">
-                      
-                  </div>
-              </section>
-          </b-carousel-item>
-        </b-carousel>
+				<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img :src="require('@/assets/razno1.jpg')" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img :src="require('@/assets/razno2.jpg')" class="d-block w-100" alt="...">
+						</div>
+						<div class="carousel-item">
+							<img :src="require('@/assets/razno3.jpg')" class="d-block w-100" alt="...">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
 			</div>
 			<div class="col-md-6 col-s-12 center-text">
 				<h3>Landscape architecture</h3>
@@ -114,21 +125,7 @@
 </body>
 </template>
 
-<script>
-export default {
-    data(){
-        return {
-            carousels: [
-                { text: 'Slide 1', src:"img/prvi.jpg" },
-                { text: 'Slide 2', src:'img/drugi.jpg' },
-                { text: 'Slide 3', color: 'success' },
-                { text: 'Slide 4', color: 'warning' },
-                { text: 'Slide 5', color: 'danger' }
-            ]
-        }
-    }
-}
-</script>
+
 
 <style>
 h2, h3, h4, h5{
